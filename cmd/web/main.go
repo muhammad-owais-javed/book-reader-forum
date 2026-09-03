b5a3741f099f9c6f158db7788dd80c8a867012eb
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-03 19:36:47 +0300

    open sqlite3 db

---
+++ cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	db, err := sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("R")
+
+}
