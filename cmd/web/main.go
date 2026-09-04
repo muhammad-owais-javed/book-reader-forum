71e73d672382a660c8310a383ad121a8ba590c2c
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-04 20:32:56 +0300

    set up application dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,9 +4,12 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"forum/cmd/web/handlers"
+	"forum/internal/repositories"
+	"forum/internal/services"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -19,16 +22,36 @@ func main() {
 	}
 	defer db.Close()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/login", handlers.LoginHandler)
+	schema, err := os.ReadFile("schema.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//creating the tables
+	_, err = db.Exec(string(schema))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// -- repos --
+	userRepository := &repositories.UserRepository{DB: db}
+	// -- services --
+	authService := &services.AuthService{Repository: userRepository}
+
+	app := &handlers.Application{
+		Auth: authService,
+	}
+
+	mux := app.Routes()
 
 	server := &http.Server{
 		Addr:         ":8080",
 		Handler:      mux,
-		ReadTimeout:  5 * time.Second,  // no default timeout for super slow requests that
-		WriteTimeout: 10 * time.Second, // could hold connections open indefinitely
+		ReadTimeout:  5 * time.Second,  // there is no default timeout for super slow requests or
+		WriteTimeout: 10 * time.Second, // super slow reading of responses that could hold connections open indefinitely, which is why these are needed
 	}
 
+	log.Printf("server running at http://localhost%s", server.Addr)
 	err = server.ListenAndServe()
 	log.Fatal(err)
 
