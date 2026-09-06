fbaade89c38c8794e0e3c9bd3ae9e348489c0783
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-06 14:17:56 +0300

    add conflict errors

--- cmd/web/handlers/errors.go
+++ cmd/web/handlers/errors.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"fmt"
+	"forum/internal/constants"
 	"log"
 	"net/http"
 )
@@ -19,8 +21,22 @@ func writeClientError(errorName string) (string, int) {
 	switch errorName {
 	case "bad request":
 		return http.StatusText(http.StatusBadRequest), http.StatusBadRequest
+
 	case "invalid email":
 		return "Error: Invalid email provided.", http.StatusBadRequest
+
+	case "too short password":
+		return fmt.Sprintf("Error: The provided password is too short. Minimum length %d characters.", constants.MinPasswordLength), http.StatusBadRequest
+
+	case "too short username":
+		return fmt.Sprintf("Error: The provided username is too short. Minimum length %d characters.", constants.MinUsernameLength), http.StatusBadRequest
+
+	case "email exists":
+		return "Error: The provided email address has already a user.", http.StatusConflict
+
+	case "username exists":
+		return "Error: The provided username is already in use.", http.StatusConflict
+
 	case "unauthorized":
 		return http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized
 
