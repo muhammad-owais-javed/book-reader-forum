30e8cbe7c860db8acf42737da013e3db29403a21
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-06 14:27:34 +0300

    add create user

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -2,3 +2,8 @@ package constants
 
 const MinUsernameLength = 3
 const MinPasswordLength = 12
+
+// ------- users table ---------
+const CheckUniqueEmail = "SELECT EXISTS(SELECT 1 FROM users WHERE email = ?"
+const CheckUniqueUserName = "SELECT EXISTS(SELECT 1 FROM users WHERE username = ?"
+const CreateUser = "INSERT INTO users (id, username, email, password_hash) VALUES ?, ?, ?, ?"
