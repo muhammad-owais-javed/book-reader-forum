7a9acc06f7566100c47e8960eb58a6c7af36c496
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-06 13:55:21 +0300

    add UUID generator

---
+++ internal/uuid/uuid.go
@@ -0,0 +1,17 @@
+package uuid
+
+import (
+	"crypto/rand"
+	"fmt"
+)
+
+func newUUID() (string, error) {
+	b := make([]byte, 16)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+
+	b[6] = (b[6] & 0xf) | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:16]), nil
+
+}
