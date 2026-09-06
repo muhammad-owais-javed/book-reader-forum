3797ea4f3476f9801ec5c9c79a719f8a14f23800
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-06 10:32:24 +0300

    add clientError for 400 and 401

--- cmd/web/handlers/errors.go
+++ cmd/web/handlers/errors.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func clientError(w http.ResponseWriter, status int) {
+
+	switch status {
+	case 400:
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	case 401:
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+	default:
+		fmt.Println("We should not have gotten here.")
+	}
+
+}
