76da3c4c8975ba40ba676752e11540eb7811b5c0
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-06 14:26:03 +0300

    fix nothing

--- cmd/web/handlers/application.go
+++ cmd/web/handlers/application.go
@@ -3,6 +3,6 @@ package handlers
 import "forum/internal/services"
 
 type Application struct {
-	Auth     *services.AuthService
-	Register *services.RegistrationService
+	Auth         *services.AuthService
+	Registration *services.RegistrationService
 }
