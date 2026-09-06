2f811173086c53f315e3cde5461a01082167b052
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-06 14:28:20 +0300

    implement global error handling

--- cmd/web/handlers/login.go
+++ cmd/web/handlers/login.go
@@ -2,8 +2,11 @@ package handlers
 
 import (
 	"context"
+	"forum/internal/constants"
 	"net/http"
+	"net/mail"
 	"time"
+	"unicode/utf8"
 )
 
 func (app *Application) LoginHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,13 +14,18 @@ func (app *Application) LoginHandler(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), 1*time.Second)
 	defer cancel()
 
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
-	userID, err := app.Auth.Authenticate(ctx, email, password)
+	email, err := mail.ParseAddress(r.PostFormValue("email"))
 	if err != nil {
-		// implement global error handling here
+		clientError(w, "invalid email")
 	}
+	password := r.PostFormValue("password")
+	if utf8.RuneCountInString(password) < constants.MinPasswordLength {
+		clientError(w, "too short password")
+		return
+	}
+
+	userID, err := app.Auth.Authenticate(ctx, email.Address, password)
+	// use global error handling here
 
 	_ = userID // --------
 	// activate generation of UUID, which will be sent to the browser
