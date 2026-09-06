e3fea40b2a612eff47cd4f3f62478243b4007ddb
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-06 15:43:17 +0300

    fix syntax

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -6,4 +6,4 @@ const MinPasswordLength = 12
 // ------- users table ---------
 const CheckUniqueEmail = "SELECT EXISTS(SELECT 1 FROM users WHERE email = ?)"
 const CheckUniqueUserName = "SELECT EXISTS(SELECT 1 FROM users WHERE username = ?)"
-const CreateUser = "INSERT INTO users (id, username, email, password_hash) VALUES ?, ?, ?, ?"
+const CreateUser = "INSERT INTO users (id, username, email, password_hash) VALUES (?, ?, ?, ?)"
