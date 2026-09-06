b654ead79a65dc1276003689053b1dafc3e8ee89
Sebastian Suchodolski <k-ssuchod@c3r1p2.hive.fi>
2026-09-06 15:07:01 +0300

    add registration page

---
+++ cmd/web/handlers/pages/register.go
@@ -0,0 +1,18 @@
+package pages
+
+import (
+	"forum/cmd/web/apperrors"
+	"html/template"
+	"net/http"
+)
+
+func RegisterPageHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, err := template.ParseFiles("ui/html/register.html")
+	if err != nil {
+		apperrors.ServerError(w, err)
+		return
+	}
+
+	tmpl.Execute(w, nil)
+}
