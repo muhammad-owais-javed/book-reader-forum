cec8d1f34b576033c349f88a56400fe5f4e1121c
Owais <contact@owais.fi>
2026-09-08 13:01:53 +0300

    UPDATE: Add CheckEmailExists() method

---
+++ internal/repository/user_repo.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"forum/internal/constants"
+	"forum/internal/models"
+)
+
+type UserRepository struct {
+	DB *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{DB: db}
+}
+
+func (r *UserRepository) CheckEmailExists(ctx context.Context, email string) (bool, error) {
+
+	var exists bool
+	err := r.DB.QueryRowContext(ctx, constants.CheckUniqueEmail, email).Scan(&exists)
+	return exists, err
+
+}
