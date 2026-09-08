b79bde24687fcc77442676e4638e667a8fd30bd1
Owais <contact@owais.fi>
2026-09-08 13:57:14 +0300

    UPDATE: connection with repository layer

--- internal/services/sessions.go
+++ internal/services/sessions.go
@@ -5,15 +5,25 @@ import (
 	"database/sql"
 	"errors"
 	"forum/internal/constants"
+	"forum/internal/repository"
 	"forum/internal/uuid"
 	"time"
 )
 
+
+type SessionService struct {
+	repo *repository.SessionRepository
+}
+
+func NewSessionService(repo *repository.SessionRepository) *SessionService {
+	return &SessionService{repo: repo}
+}
+
 // creates a session with a UUID, userID and expiration. Returns the UUID
-func CreateSession(ctx context.Context, tx *sql.Tx, userID string) (string, error) {
+func (s *SessionService) CreateSession(ctx context.Context, userID string) (string, error) {
 
 	// ---- generate UUID -------
-	UUID, err := uuid.NewUUID()
+	sessionID, err := uuid.NewUUID()
 	if err != nil {
 		return "", err
 	}
@@ -22,20 +32,23 @@ func CreateSession(ctx context.Context, tx *sql.Tx, userID string) (string, erro
 	expiresAt := time.Now().Add(constants.SessionExpiry * time.Minute).Format("2006-01-02 15:04:05")
 
 	// ---- add session -------
-	_, err = tx.ExecContext(ctx, constants.AddSession, UUID, userID, expiresAt)
+	// _, err = tx.ExecContext(ctx, constants.AddSession, UUID, userID, expiresAt)
+	err = s.repo.InsertSession(ctx, sessionID, userID, expiresAt)
 	if err != nil {
 		return "", err
 	}
 
-	return UUID, nil
+	return sessionID, nil
 }
 
 // validates that a given session id has a valid session in the db that has not expired, returns bool
 func ValidateSession(ctx context.Context, tx *sql.Tx, sessionID string) (bool, error) {
 
-	var expiresAt string
+	// var expiresAt string
+	// err := tx.QueryRowContext(ctx, constants.GetExpiryTime, sessionID).Scan(&expiresAt)
+	
+	expiresAt, err := s.repo.GetSessionExpiry(ctx, sessionID)
 
-	err := tx.QueryRowContext(ctx, constants.GetExpiryTime, sessionID).Scan(&expiresAt)
 	if errors.Is(err, sql.ErrNoRows) {
 		return false, nil
 	}
