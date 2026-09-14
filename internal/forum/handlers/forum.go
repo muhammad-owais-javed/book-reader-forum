1dea2326714c532659c18b7db04e828ad9a3f6bb
Owais <contact@owais.fi>
2026-09-14 17:23:23 +0300

    hello.go > forum.go: add ViewForum() hhandler

---
+++ internal/forum/handlers/forum.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"net/http"
+ )
+
+
+type ForumHandler struct {
+
+	PostService *services.PostService
+
+}
+
+
+func NewForumHandler() *ForumHandler {
+	
+	return &ForumHandler{PostService: postService}
+	//return &ForumHandler{}
+}
+
+func (h *ForumHandler) ViewForum(w http.ResponseWriter, r *http.Request ) {
+	ctx := r.Context()
+	
+	posts, err := h.PostService.GetAllPosts(ctx)
+	if err != nil {
+		http.Error(w, "Failed to load posts", http.StatusInternalServerError )
+		return
+	}
+
+	// For now, just print the number of posts to the screen to test it!
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(fmt.Sprintf("Welcome to the Forum! There are %d posts.", len(posts))))
+}
+
+
+// func (h *ForumHandler) HelloWorld(w http.ResponseWriter, r *http.Request ) {
+	
+// 	w.Header().Set("Content-Type", "text/plain")
+// 	w.Write([]byte("Hello World! Welcome to the protected Forum!"))
+
+// }
