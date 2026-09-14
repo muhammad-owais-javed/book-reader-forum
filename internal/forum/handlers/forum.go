99334f9f11fa3742a6576147cf8f8336d684bc68
Owais <contact@owais.fi>
2026-09-14 17:52:31 +0300

    UPDATE: Add CreatePost() method

--- internal/forum/handlers/forum.go
+++ internal/forum/handlers/forum.go
@@ -53,6 +53,36 @@ func (h *ForumHandler) ViewForum(w http.ResponseWriter, r *http.Request ) {
 }
 
 
+func (h *ForumHandler) CreatePost(w http.ResponseWriter, r *http.Request ) {
+	
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed )
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest )
+		return
+	}
+
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+
+	// TODO: We need the REAL logged-in user ID here!
+	userID := "dummy-user-id" 
+
+	ctx := r.Context()
+	err = h.PostService.CreatePost(ctx, userID, title, content)
+	if err != nil {
+		http.Error(w, "Failed to create post", http.StatusInternalServerError )
+		return
+	}
+
+	http.Redirect(w, r, "/forum", http.StatusSeeOther )
+}
+
+
 // func (h *ForumHandler) HelloWorld(w http.ResponseWriter, r *http.Request ) {
 	
 // 	w.Header().Set("Content-Type", "text/plain")
